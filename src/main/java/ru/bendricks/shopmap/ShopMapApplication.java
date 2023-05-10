ea8f40fdfd69a4ca940a7eec8e6ca9e47206b23a
BendRicks <bendricks@yandex.ru>
2023-05-10 22:07:53 +0300

    fix(Project): Fix
    Simple file fixes

--- src/main/java/ru/bendricks/shopmap/ShopMapApplication.java
+++ src/main/java/ru/bendricks/shopmap/ShopMapApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShopMapApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShopMapApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShopMapApplication.class, args);
+    }
 
 }
