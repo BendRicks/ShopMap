e58a07aa33391e31c46c25b19f38b1ce32d6d2df
BendRicks <bendricks@yandex.ru>
2023-05-10 22:01:33 +0300

    fix(Repostitory): Added methods
    Added working methods to ShopRepository

--- src/main/java/ru/bendricks/shopmap/repository/ShopRepository.java
+++ src/main/java/ru/bendricks/shopmap/repository/ShopRepository.java
@@ -1,2 +1,21 @@
-package ru.bendricks.shopmap.repository;public class ShopRepository {
+package ru.bendricks.shopmap.repository;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import ru.bendricks.shopmap.entity.Shop;
+import ru.bendricks.shopmap.entity.ShopStatus;
+import ru.bendricks.shopmap.entity.User;
+
+import java.util.List;
+
+public interface ShopRepository extends JpaRepository<Shop, Integer> {
+
+    Integer countByCreator(User user);
+    @Query(value = "select distinct sh from Shop sh inner join fetch Address adr on sh.id=adr.shop.id where sh.shopStatus=:status and (sh.name like %:nameLike% or sh.description like %:nameLike% or adr.city like %:nameLike% or adr.district like %:nameLike% or adr.street like %:nameLike% or adr.building like %:nameLike% or adr.room like %:nameLike%)")
+    List<Shop> findAllByString(String nameLike, ShopStatus status, Pageable pageable);
+
+    @Query(value = "select count(distinct sh) from Shop sh inner join fetch Address adr on sh.id=adr.shop.id where sh.shopStatus=:status and (sh.name like %:nameLike% or sh.description like %:nameLike% or adr.city like %:nameLike% or adr.district like %:nameLike% or adr.street like %:nameLike% or adr.building like %:nameLike% or adr.room like %:nameLike%)")
+    Integer getMatchesAmount(String nameLike, ShopStatus status);
+
 }
