00ae8367141869c1b1a67e9f2ffb2bba3e293495
BendRicks <bendricks@yandex.ru>
2023-05-10 21:57:11 +0300

    fix(Util): Fixed util classes
    Fixed util classes to match project requirements

--- src/main/java/ru/bendricks/shopmap/util/constraints/GeneralConstants.java
+++ src/main/java/ru/bendricks/shopmap/util/constraints/GeneralConstants.java
@@ -8,4 +8,11 @@ public class GeneralConstants {
     public static final String BEARER_HEADER_TOKEN = "Bearer ";
     public static final int JWT_START_INDEX = 7;
 
+    @UtilityClass
+    public class SQLStateConstants {
+
+        public static final String UNIQUE_CONSTRAINT = "23505";
+
+    }
+
 }
